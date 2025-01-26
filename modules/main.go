a7afe09a429460b23d50099dc933c5b8071d15fd
deepak <darshandeepak1483@gmail.com>
2025-01-26 12:37:11 +0530

    map,arr,slice,make,struct

--- modules/main.go
+++ modules/main.go
@@ -27,6 +27,9 @@ func main() {
 	image := Image{FileName: "abc.png"}
 	printDocumentAndImage(doc)
 	printDocumentAndImage(image)
+
+	arr_slice_make_map()
+	struct_eg()
 }
 
 // Empty interfaces
@@ -79,6 +82,7 @@ type Image struct {
 	FileName string
 }
 
+// Methods on struct
 func (d Document) Print() string {
 	return "Printing document = " + d.Title
 }
@@ -91,3 +95,88 @@ func printDocumentAndImage(p Printer) {
 	fmt.Println(p.Print())
 }
 
+func arr_slice_make_map() {
+	arr := [5]int{1,2,3,4,5}
+	fmt.Println(arr)
+
+	// Empty array
+	var em_ar [4]int
+	fmt.Println("Empty array = ",em_ar)
+	em_ar[0] = 5
+	em_ar[2] = 10
+	fmt.Println("array = ",em_ar)
+
+	for i,val := range arr {
+		fmt.Printf("Index %d Value %d \n",i,val)
+	}
+
+	slice1 := arr[0:4]
+	fmt.Println(slice1)
+
+	slice2 := arr[3:5]
+	fmt.Println(slice2)
+
+	// changeing 3rd index will also change in slice1 and slice2 and in the array
+	slice1[3] = 14
+	fmt.Println(arr)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+
+	// creating slice with make()
+
+	nums := make([]int,0,10)
+	fmt.Println(nums)
+
+	for i:=0;i<6;i++ {
+		 // nums[i] = i -> will return index out of range
+		 nums = append(nums, i) // will return a new slice when capacity is reached
+		 fmt.Printf("Length : %d Capacity %d \n",len(nums),cap(nums))
+	}
+
+	fmt.Println(nums)
+
+	// Maps
+
+	var map1 map[int]string
+	fmt.Println(map1) // Empty map
+
+	capital := make(map[string]string)
+
+	capital["TamilNadu"] = "Chennai"
+	capital["Karnataka"] = "Bangalore"
+
+	for key,val := range capital {
+		fmt.Printf("Key = %s and Value = %s \n",key,val)
+	}
+
+	// Check if key exists
+
+	value,exist := capital["Kerala"]
+
+	if exist {
+		fmt.Println("value exist ",value)
+	} else {
+		fmt.Println("Value does not exist")
+	}
+
+	// Delete a key
+	delete(capital,"Karnataka")
+	fmt.Println(capital)
+
+	// makes() is used to initialise maps,slice and channels
+} 
+
+
+func struct_eg() {
+	// Anonymous struct
+
+	animal := struct {
+		Name string
+		isDomesticated bool
+	} {
+		Name: "Lion",
+		isDomesticated: false,
+	}
+
+	fmt.Println(animal)
+}
